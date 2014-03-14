1d606201679929223671abefd4e6952acdb5398d
Alex Notov <anotov@gmail.com>
2014-03-14 07:31:18 -0700

    When given a word to learn, it should attempt to add suffixes

--- spec/javascripts/suffix_tree_spec.js
+++ spec/javascripts/suffix_tree_spec.js
@@ -1,6 +1,20 @@
 require('../../suffix_tree.js');
 describe("SuffixTree", function() {
+  var t;
+  beforeEach(function() {
+    t = new SuffixTree();
+  });
   it("can be initialized", function() {
-    var t = new SuffixTree();
+    expect(t).toEqual(jasmine.any(SuffixTree));
+  });
+
+  describe("given the word banana to learn", function() {
+    beforeEach(function() {
+      spyOn(t, 'add');
+      t.learn('banana');
+    });
+    it("invokes the add function on '$'", function() {
+      expect(t.add).toHaveBeenCalledWith('$');
+    });
   });
 });
