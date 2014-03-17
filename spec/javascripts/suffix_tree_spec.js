9f3360ef8388a59497900ca1bfc96d92eea495fb
SylvieC <sylviecottrell@gmail.com>
2014-03-16 21:40:11 -0700

    all spec passing

--- spec/javascripts/suffix_tree_spec.js
+++ spec/javascripts/suffix_tree_spec.js
@@ -20,30 +20,43 @@ describe("SuffixTree", function() {
       t.learn('banana');
     });
 
+    // it("contains the delimiter node", function() {
+    //   expect(t.$).toEqual(jasmine.any(SuffixTree));
+    // });
+
     it("invokes the add function for each delimited suffix of the word",
       function() {
         expect(
           t.add.calls.map(
-            function(spy_call){return spy_call.args;}
+            function(spy_call) {
+              return spy_call.args;
+            }
           )
         ).toEqual(
-          [['a$'], ['na$'], ['ana$'], ['nana$'], ['anana$'], ['banana$']]
+          [
+            ['a$'],
+            ['na$'],
+            ['ana$'],
+            ['nana$'],
+            ['anana$'],
+            ['banana$']
+          ]
         );
       }
     );
   });
 
-  describe("learning banana suffix by suffix",function() {
-    it("learns a",function() {
+  describe("learning banana suffix by suffix", function() {
+    it("learns a", function() {
       t.learn('a');
       expect(t.a$).toEqual(jasmine.any(SuffixTree));
     });
-    it("learns na after a",function() {
+    it("learns na after a", function() {
       t.learn('a');
       t.learn('na');
       expect(t.na$).toEqual(jasmine.any(SuffixTree));
     });
-    it("learns ana after na",function() {
+    it("learns ana after na", function() {
       t.learn('a');
       t.learn('na');
       t.learn('ana');
@@ -51,7 +64,7 @@ describe("SuffixTree", function() {
       expect(t.na$).toEqual(jasmine.any(SuffixTree));
       expect(t.a.na$).toEqual(jasmine.any(SuffixTree));
     });
-    it("learns nana after ana",function() {
+    it("learns nana after ana", function() {
       t.learn('a');
       t.learn('na');
       t.learn('ana');
@@ -61,7 +74,7 @@ describe("SuffixTree", function() {
       expect(t.na.$).toEqual(jasmine.any(SuffixTree));
       expect(t.na.na$).toEqual(jasmine.any(SuffixTree));
     });
-    it("learns anana after nana",function() {
+    it("learns anana after nana", function() {
       t.learn('a');
       t.learn('na');
       t.learn('ana');
@@ -73,7 +86,7 @@ describe("SuffixTree", function() {
       expect(t.a.na.$).toEqual(jasmine.any(SuffixTree));
       expect(t.a.na.na$).toEqual(jasmine.any(SuffixTree));
     });
-    it("learns banana after anana",function() {
+    it("learns banana after anana", function() {
       t.learn('a');
       t.learn('na');
       t.learn('ana');
--- suffix_tree.js
+++ suffix_tree.js
@@ -1,6 +1,5 @@
 SuffixTree = (function() {
-  function SuffixTree() {
-  }
+  function SuffixTree() {}
 
   Object.defineProperty(SuffixTree, "delimiter", {
     // the property created here is used in server.js
@@ -41,8 +40,7 @@ SuffixTree = (function() {
   SuffixTree.prototype.suffixes = function() {
     return _.keys(
       _.omit(
-        this,
-        ['discovered','suffixes','search','leaves','learn','add','is_leaf']
+        this, ['discovered', 'suffixes', 'search', 'leaves', 'learn', 'add', 'is_leaf', 'matching_key']
       )
     );
   };
@@ -63,6 +61,48 @@ SuffixTree = (function() {
   //
   //  an edge is a key of the node
   SuffixTree.prototype.search = function(query) {
+    query = query.remove(SuffixTree.delimiter);
+
+    if (query.length === 0) {
+      // if query is empty return undefined
+      return void 0;
+    }
+
+    // a query is a substring of s iff it's a prefix
+    // of a suffix of s
+    SuffixTree.results = [];
+
+    var node, suffix = "";
+
+    // go through the keys of the current node
+    for (var i = 0, keys = this.suffixes(); i < keys.length; i++) {
+      if (keys[i][0] === query[0]) {
+        // if any of the current keys starts with
+        // the first character of the query string
+        // we need to check if this key contains 
+        // the query. if so, we are on an exact match
+        // and don't need to traverse further
+        if (keys[i].has(query)) {
+          // if current edge (key) contains query
+          node = this[keys[i]];
+          // if the current edge contains query, we are 
+          // on an edge leading to a leaf
+          suffix = keys[i];
+        } else if (query.has(keys[i]) && !keys[i].has(SuffixTree.delimiter)) {
+          // if query contains key, and key does not point to a leaf 
+          // perform the search on the unmatched part of the string
+          // on the node pointed to by the key (traverse down)
+          node = this[keys[i]].search(query.remove(keys[i]));
+        }
+      }
+    }
+
+    if (node instanceof SuffixTree) {
+      // if we have a node, let's grab its leaves
+      // pass in SuffixTree.results and the suffix
+      node.leaves(SuffixTree.results, suffix);
+    }
+    return SuffixTree.results;
   };
 
   // Return the leaf nodes for the sub tree
@@ -70,6 +110,19 @@ SuffixTree = (function() {
   // you should consider implementing a depth
   // first traversal to the leaf nodes
   SuffixTree.prototype.leaves = function(leaves) {
+    _.each(this.suffixes(), function(suffix) {
+      this[suffix].leaves(leaves, suffix);
+    }, this);
+
+    if (this.is_leaf()) {
+      leaves.push(_.extend(this, {
+        filename: SuffixTree.delimiter_filename_hash[_suffix.last(1)]
+      }));
+    }
+
+    return leaves;
+
+
   };
 
   // This should be the entry point into the suffix
@@ -81,14 +134,80 @@ SuffixTree = (function() {
   // However, this should work just as easily for a simple
   // word, like banana :)
   // the meat and potatoes are cooked by SuffixTree.prototype.add
-  SuffixTree.prototype.learn = function(suffix) {
+
+  SuffixTree.prototype.learn = function(word) {
+    //iterate over word and for each suffix of word, call add function
+    //the learn function is called on the root node
+
+    if (this.$ === undefined) {
+      this.$ = new SuffixTree();
+    }
+
+    word += "$";
+
+    var suffix = "$";
+
+    for (var i = word.length - 2; i >= 0; i--) {
+      suffix = word[i] + suffix;
+      this.add(suffix);
+    }
+  };
+  // this function returns the key that starts with the same letter as the word or the number 0 if there are none
+  SuffixTree.prototype.matching_key = function(word) {
+    var suffixes = this.suffixes();
+    for (var i = 0; i < suffixes.length; i++) {
+      var suffix = suffixes[i];
+      if (suffix[0] === word[0]) {
+        return suffix;
+      }
+    }
+    return null;
+  };
+  // method to count how many letters two words start with in common
+  var num_begin_match = function(word1, word2) {
+    for (var i = 0; i < word1.length && i < word2.length; i++) {
+      if (word1[i] !== word2[i]) {
+        return i;
+      }
+    }
+    return Math.min(word1.length, word2.length);
+  };
+
+  var add = function(a, b) {
+    return a + b;
   };
 
   // this is the meat and potatoes function that cooks this excellent meal
   SuffixTree.prototype.add = function(suffix) {
+
+    var length = suffix.length;
+
+    var key = this.matching_key(suffix);
+
+    if (key === null) {
+      this[suffix] = new SuffixTree();
+    } else if (key !== '$') {
+      //we are in this part of the loop when there is already a key starting with the same letter as suffic
+      //key is the key of the node
+      var num_match = num_begin_match(suffix, key);
+
+      var new_key = suffix.substring(0, num_match);
+      var left_key = key.substring(num_match, key.length);
+      var right_key = suffix.substring(num_match, suffix.length);
+
+      var node = this[key];
+      delete this[key];
+      this[new_key] = node;
+
+      if (left_key !== '') {
+        node.add(left_key);
+      }
+      if (right_key !== '') {
+        node.add(right_key);
+      }
+    }
   };
 
   // this just returns the SuffixTree to the outside world for usage
   return SuffixTree;
 })();
-
